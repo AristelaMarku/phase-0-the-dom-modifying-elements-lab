f6fb271fdb3c272059bbae0d7d283d2c8f02d42d
Aristela Marku <aristela.marku@gmail.com>
2021-09-07 23:20:18 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const body=document.getElementsByTagName('body')[0]
+const newHeader=document.createElement('h1')
+body.appendChild(newHeader)
+
+newHeader.id='victory'
+
+
+newHeader.innerHTML="YOUR-NAME is the champion "
